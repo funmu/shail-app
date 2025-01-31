8728fc5e9f4f35ae990349979b3773abb1f39aaa
funmu <muralirk@gmail.com>
2025-01-30 17:42:54 -0800

    change name to "Shail"; add fishbowl logo

--- app.config.js
+++ app.config.js
@@ -2,12 +2,38 @@
 export default {
     expo: {
       // ... other config options ...
-      scheme: "your-scheme", // Add this line
+      name: "Shail",
+      scheme: "shail", // Add this line
+      "slug": "shail",
+      "version": "1.0.0",
+      "orientation": "portrait",
+      "icon": "./assets/images/fishbowl.icon.png",
+      "userInterfaceStyle": "automatic",
+      "newArchEnabled": true,
+      "splash": {
+        "image": "./assets/images/splash.png",
+        "resizeMode": "cover",
+        "backgroundColor": "#000000"
+      },
+      "assetBundlePatterns": ["**/*"],
+
       web: {
         bundler: "metro"
       },
+
       extra: {
         clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
       },
-    }
+
+      ios: {
+        "supportsTablet": false,
+        "bundleIdentifier": "com.shail.app",
+        "config": {
+          "usesNonExemptEncryption": false
+        },
+      }
+    }  
   };
+
+  
+  
